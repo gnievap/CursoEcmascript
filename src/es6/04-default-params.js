dd8963e03d3e25a4c2f9a3d1d3f7546608066429
Gaby Nieva <gaby.nieva@outlook.com>
2022-11-08 22:04:27 -0600

    ES6: Asignación de desestructuración

--- src/es6/04-default-params.js
+++ src/es6/04-default-params.js
@@ -3,4 +3,15 @@ function newUser(name, age, country){
     var name = name || 'Gabs';  
     var age = age || 34;
     var country = country || 'MX';
+    console.log(name, age, country);
 }
+
+newUser();
+newUser('Gings', 7, 'MX');
+
+function newAdmin(name = 'Vamp', age=32, country='CA' ){
+    console.log(name, age, country);
+}
+
+newAdmin();
+newAdmin('Ana', 28, 'PE');
