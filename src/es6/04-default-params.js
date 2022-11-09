d9a77af332fbbbe3fb91b0c978ccf9158a525dc5
Gaby Nieva <gaby.nieva@outlook.com>
2022-11-08 21:32:56 -0600

    ES6: Parámetros por defecto

---
+++ src/es6/04-default-params.js
@@ -0,0 +1,6 @@
+function newUser(name, age, country){
+    //la var.local name es igual al parámetro o a su valor por defaul 'Gabs'
+    var name = name || 'Gabs';  
+    var age = age || 34;
+    var country = country || 'MX';
+}
