d524ac6dd16b79902c21d1d1e76ecb68355759f5
Gaby Nieva <gaby.nieva@outlook.com>
2023-05-11 23:54:40 -0600

    ES12: separator y replaceAll

---
+++ src/es12/00-numeric-separators.js
@@ -0,0 +1,5 @@
+// separadores numéricos
+// lecturas amigables de valores numéricos
+
+const value = 100_000_000_000_000;
+console.log(value);
---
+++ src/es12/01-replaceAll.js
@@ -0,0 +1,4 @@
+const string = "JavaScript es un lenguaje de programación muy extraño ";
+console.log( string );
+const replacedString = string.replace("JavaScript", "TypeScript");
+console.log( replacedString);
