9d5c4df4aae3b5ab285345d25dab82aca8f7ea57
Gaby Nieva <gaby.nieva@outlook.com>
2023-05-12 22:12:49 -0600

    ES13: at

---
+++ src/es13/00-at.js
@@ -0,0 +1,6 @@
+const array = [ "one", "two", "three", "four", "five", "six" ];
+
+// imprimir el último elemento sin saber cuántos elementos hay
+console.log( array[array.length - 1] ); 
+
+console.log( array.at(-1) );
